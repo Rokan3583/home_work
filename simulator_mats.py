7c596f0a8055edfba01e6c008cc0e4a496b2442c
Rokan3583 <joskiytelik@gmail.com>
2024-10-03 18:02:45 +0300

    Ubdate

--- simulator_mats.py
+++ simulator_mats.py
@@ -2,7 +2,6 @@
 from random import randint
 student = input('Представьтесь, пожалуйста: ')
 try:
-
     level = int(input('Выберите уровень сложности 1 - 5: '))
 except:
     level - 1
@@ -10,15 +9,15 @@ except:
 if level < 1 or level > 5:
     level= 1
     print('Установлен первый уровень сложности.')
-print(f'Хорошо, (student). Тебе задачка.')
+print(f'Хорошо, {student}. Тебе задачка.')
 min = 10 ** (level - 1)
 max = 10 ** level - 1
 a = randint (min, max)
 b =randint (min, max)
-print(f'(student), сколько будет {a} + {b}? ', end='')
+print(f'{student}, сколько будет {a} + {b}? ', end='')
 correct_answer = a + b
 student_answer = input()
 if student_answer == str(correct_answer):
-    print(f'Ax, ну какой умница, (student). Это пять!')
+    print(f'Ax, ну какой умница, {student}. Это пять!')
 else:
     print(f'Два, {student}, можешь садиться! Правильный ответ {correct_answer}.')
